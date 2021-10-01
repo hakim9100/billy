5a20ded9d70bf5c7580ac8dd4d6168694756c2c2
hakim9100 <tekno9100@live.fr>
2021-10-01 15:02:18 +0200

    Second

--- src/Controller/SioController.php
+++ src/Controller/SioController.php
@@ -9,20 +9,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SioController extends AbstractController {
+class SioController extends AbstractController
+{
 
     /**
-     * @Route("/lucky/hello", methods={"GET"}, name="Hello_World")
+     * @Route("/sio/hello", methods={"GET"}, name="Hello_World")
      */
 
-    public function indexAction(Request $request): Response{
+    public function indexAction(Request $request): Response
+    {
         $hello = "Man";
-    $languages = $request -> getLanguages();
-    $id = $request -> getClientIp();
-        return $this->render('lucky/Hello.html.twig', [
+        $languages = $request->getLanguages();
+        $id = $request->getClientIp();
+        return $this->render('sio/Hello.html.twig', [
             'Hello' => $hello,
             'languages' => $languages,
             'ip' => $id,
+        ]);
+
+    }
+
+    /**
+     * @Route("/Hello/{nom}", methods={"GET"}, name="sio_hello")
+     */
+    public function helloAction($nom = 'Hello Inconnu', Request $request)
+    {
+        $ip = $request->getClientIp();
+        $this->addFlash('succes', "Bienvenu :" . $ip);
+
+        $noms = explode("_", $nom);
+        if (count($noms) == 1) {
+            $noms[1] = null;
+        }
+
+        return $this->render('sio/Name.html.twig', [
+            'nom' => $noms,
+            'ip' => $ip,
         ]);
     }
 }
